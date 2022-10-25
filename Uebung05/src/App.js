34ea750a47b886380962ce40584ead7e998e8e4d
MartinaMeyerX <martina.meyer@students.fhnw.ch>
2022-10-25 21:58:22 +0200

    Übung 5

---
+++ Uebung05/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Timer from "./Timer";
+
+
+function App() {
+
+
+    return (<>
+                <h2>Countdown von 50</h2>
+                <hr/>
+                <Timer countdown="50"/>
+                <hr/>
+            </>);
+}
+
+export default App;
