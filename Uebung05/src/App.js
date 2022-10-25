f573408ccab59dfb09a9f7cf9d7115feca85f854
MartinaMeyerX <martina.meyer@students.fhnw.ch>
2022-10-25 22:01:41 +0200

    Übung5

--- Uebung05/src/App.js
+++ Uebung05/src/App.js
@@ -1,16 +1,24 @@
-import React from "react";
+import React from 'react';
+import Typography from "@mui/material/Typography";
 import Timer from "./Timer";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
 
 
 function App() {
 
+  return (
+    <>
+      <AppBar position="sticky" color="primary">
+        <Toolbar>
+          <Typography variant="h6">Counter</Typography>
+        </Toolbar>
+      </AppBar>
 
-    return (<>
-                <h2>Countdown von 50</h2>
-                <hr/>
-                <Timer countdown="50"/>
-                <hr/>
-            </>);
+      <Timer/>
+    </>
+  );
+
 }
 
 export default App;
--- Uebung05/src/Timer.js
+++ Uebung05/src/Timer.js
@@ -1,43 +1,59 @@
-import React, {Component} from "react";
+import React from "react";
+import Grid from '@mui/material/Grid';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import { clear } from "@testing-library/user-event/dist/clear";
+import { Typography } from "@mui/material";
 
-class Timer extends Component {
+
+var timer = ""
+
+class Timer extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = {count: 50};
-        // Event Handler
-        this.decrease = this.decrease.bind(this);
-        this.countdown = this.countdown.bind(this);
+        this.state = {time: timer, render: true}
+        this.updateTime = this.updateTime.bind(this);
+        this.buttonclicked = this.buttonclicked.bind(this);
     }
 
-    decrease() {
-        this.setState({count: this.state.count - 1});
-        if (this.state.count == 1) {
-            this.setState = ({count: "FERTIG"}) }
-        if (this.state.count == "FERTIG") {
-            this.setState = ({count: "FERTIG"}) }
+    buttonclicked(event) {
+        if (timer % 1 === 0){
+            clearInterval(clear);
+            this.setState({time: timer, render: false});
+            clear = setInterval(this.updateTime, 1000);
+        }
     }
 
-    countdown() {
-        this.state = {count: 51};
-        // Event Handler
-        this.decrease = this.decrease.bind(this);
+    resetTimer(event) {
+        timer = event.target.value;
     }
 
-    componentDidMount() {
-        this.interval = setInterval(this.decrease, 1000);
-    }
-    componentWillUnmount() {
-        clearInterval(this.interval);
+    updateTime() {
+        if (this.state.time > 1) {
+            this.setState({time: this.state.time - 1});
+        }
+        else {
+            this.setState({time:"FERTIG", render: true});
+        }
     }
 
     render() {
-        return(<>
-            <p>{this.state.count}</p>
-            <button onClick={this.countdown}>Start</button>
+        return (
+        <>
+        {this.state.render &&
+            <Grid container style={{margin: 8}}>
+                <TextField placeholder={'Geben Sie eine Zahl ein'} type = "number" step = "1" onChange={this.resetTimer}/>
+            </Grid>
+        }
+        <Grid container style={{margin:10}}>
+            <Typography variant="h5" align="left">{this.state.time}</Typography>
+        </Grid>
+        <Grid container style={{margin:8}}>
+            <Button variant="contained" onClick={this.buttonclicked}>Start</Button>
+        </Grid>
         </>)
     }
-
 }
 
 export default Timer;
